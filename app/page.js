e86027f0c06dba8d81eb17633f1590ebfba7a74e
jameslowry60 <jameslowry1960@gmail.com>
2026-06-26 16:16:57 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -144,7 +144,7 @@ const [passport, setPassport] = useState({
 <input placeholder="Consultant" />
 <input placeholder="Emergency Contact" />
   </div>
-    )}
+    
       <div style={section}>
         <div style={title}>Share Awareness</div>
 
@@ -205,6 +205,7 @@ const [passport, setPassport] = useState({
           Join
         </button>
       </div>
+      )}
     </div>
   );
 }
